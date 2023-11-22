b6bbb4bb0ead65101d56063378faca0b15b282b9
NPS <nicolaspetri18@gmail.com>
2023-11-22 15:33:09 -0300

    isso

--- usuarios.js
+++ usuarios.js
@@ -1,77 +1,77 @@
-// 'use strict'
+'use strict'
 
-// async function listarUsuarios() {
-//     const url = 'http://10.107.140.2:8080/usuarios/'
-//     const response = await fetch(url)
-//     const data = await response.json()
-//     console.log(data)
+async function listarUsuarios() {
+    const url = 'http://10.107.140.2:8080/usuarios/'
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
 
-//     return data
-// }
+    return data
+}
 
-// // async function cadastrarUsuario(usuario) {
-// //     const url = 'http://10.107.140.2:8080/usuarios/'
-// //     const options = {
-// //         method: 'POST',
-// //         headers: {
-// //             'Content-Type': 'application/json'
-// //         },
-// //         body: JSON.stringify(usuario)
-// //     }
+async function cadastrarUsuario(usuario) {
+    const url = 'http://10.107.140.2:8080/usuarios/'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    }
 
-// //     const response = await fetch(url, options)
-// //     console.log(response.ok)
+    const response = await fetch(url, options)
+    console.log(response.ok)
 
-// //     return response.ok
-// // }
+    return response.ok
+}
 
-// async function atualizarUsuario(usuario) {
-//     const url = `http://10.107.140.2:8080/usuarios/${usuario.id}`
-//     const options = {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(usuario)
-//     }
+async function atualizarUsuario(usuario) {
+    const url = `http://10.107.140.2:8080/usuarios/${usuario.id}`
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    }
 
-//     const response = await fetch(url, options)
-//     console.log(response.ok)
-//     return response.ok
+    const response = await fetch(url, options)
+    console.log(response.ok)
+    return response.ok
 
-// }
-// // async function deletarUsuario(usuario) {
-// //     const url = `http://10.107.140.2:8080/usuarios/${usuario.id}`
-// //     const options = {
-// //         method: 'DELETE',
-// //         headers: {
-// //             'Content-Type': 'application/json'
-// //         },
-// //         body: JSON.stringify(usuario)
-// //     }
+}
+async function deletarUsuario(usuario) {
+    const url = `http://10.107.140.2:8080/usuarios/${usuario.id}`
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    }
 
-// //     const response = await fetch(url, options)
-// //     console.log(response.ok)
-// //     return response.ok
-// // }
+    const response = await fetch(url, options)
+    console.log(response.ok)
+    return response.ok
+}
 
-// //local de testes
+// local de testes
 
-// // const novoUsuario = {
-// //     nome: 'Nicolas Vasconcelos',
-// //     email: 'nicolas@gmail.com'
-// // }
+const novoUsuario = {
+    nome: 'Nicolas Vasconcelos',
+    email: 'nicolas@gmail.com'
+}
 
-// const usuarioAtualizado = {
-//     id: '48',
-//     nome: 'Luan DSO',
-//     email: 'luan@dso.com'
-// }
+const usuarioAtualizado = {
+    id: '48',
+    nome: 'Luan DSO',
+    email: 'luan@dso.com'
+}
 
-// // deletarUsuario(usuarioAtualizado)
+// deletarUsuario(usuarioAtualizado)
 
-// // atualizarUsuario(usuarioAtualizado)
+// atualizarUsuario(usuarioAtualizado)
 
-// // // cadastrarUsuario(novoUsuario)
+// // cadastrarUsuario(novoUsuario)
 
-// // listarUsuarios()
+// listarUsuarios()
